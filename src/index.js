0656539322236b274aca12d7a561141af27628a5
Gea-Suan Lin <gslin@gslin.org>
2024-09-30 06:44:02 +0800

    Write with fp.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ const axios = require('axios');
 const fp = require('lodash/fp');
 
 const neko = async () => {
-  return axios.get('https://nekos.best/api/v2/neko');
+  return axios.get('https://nekos.best/api/v2/neko?amount=2').then(
+    fp.flow(
+      fp.get('data.results'),
+      fp.map('url'),
+    ),
+  );
 };
 
 (async () => {
